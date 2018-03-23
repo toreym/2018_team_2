db8a4578d625a68188dbfe7b168f49c6ddc198c0
Luke Stadtler <luke@rocketreferrals.com>
2018-03-23 02:32:29 -0500

    adding random stuff

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,13 @@ Rails.application.routes.draw do
   root to: "home#index"
 
   resources :funding_needs do
-    
+
   end
+  #TODO: add interests model that will store
+  #all the known interests
+  #probably remove some of those fields from fields_of_interest
+  # post 'interests/:id/like/:organization_id'
+  # post 'interests/:id/dislike/:organization_id'
+  # get '/search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
