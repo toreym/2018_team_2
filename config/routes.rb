f3660290a58256f7e241618f2b8bd29914e985b0
Luke Stadtler <luke@rocketreferrals.com>
2018-03-23 01:54:35 -0500

    add funding needs route

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   devise_for :donors
   devise_for :organizations
   root to: "home#index"
+
+  resources :funding_needs do
+    
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
