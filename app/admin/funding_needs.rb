ff2d3e2ea8b10d6ea482efb73f654ecc745585f6
Torey Maerz <torey@rocketreferrals.com>
2018-03-24 02:08:23 -0500

    Multiple interests per funding need

--- app/admin/funding_needs.rb
+++ app/admin/funding_needs.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register FundingNeed do
   permit_params :name, :organization_id, :description, :website, :goal, :end_date, :image_url, :primary_contact_name,
-                :primary_contact_email, :primary_contact_phone, :approved, :image
+                :primary_contact_email, :primary_contact_phone, :approved, :image, interest_ids: []
 
   form do |f|
     f.inputs 'FundingNeed' do
@@ -14,6 +14,7 @@ ActiveAdmin.register FundingNeed do
       f.input :primary_contact_email
       f.input :primary_contact_phone
       f.input :approved
+      f.input :interests, hint: "Hold CTRL and click to select more than one item."
       f.input :image
     end
     f.actions
@@ -24,7 +25,7 @@ ActiveAdmin.register FundingNeed do
       row "Image" do |f|
         image_tag(f.image.url(:thumb))
       end
-      rows :name, :organization, :approved, :primary_contact_email, :primary_contact_name, :primary_contact_phone, :description
+      rows :name, :organization, :approved, :primary_contact_email, :primary_contact_name, :primary_contact_phone, :description, :interests
     end
   end
 
@@ -35,6 +36,7 @@ ActiveAdmin.register FundingNeed do
     column :primary_contact_email
     column :primary_contact_name
     column :primary_contact_phone
+    column :interests
     actions
   end
 end
