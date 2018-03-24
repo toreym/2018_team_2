69c93cdc5774b90f0fbd8bcbbd9cb338cfe571f1
Torey Maerz <torey@rocketreferrals.com>
2018-03-24 12:39:43 -0500

    Fixes

--- app/admin/myorg/funding_needs.rb
+++ app/admin/myorg/funding_needs.rb
@@ -9,7 +9,12 @@ ActiveAdmin.register FundingNeed, namespace: :myorg do
 
   form do |f|
     f.inputs 'Funding Need' do
-      f.input :organization
+      if current_organization_user.organizations.count > 1
+        f.input :organization, :label => 'Organization', :as => :select, :collection => current_organization_user.organizations.map{|o| [o.name, o.id]}
+      else
+        f.input :organization_id, :as => :hidden, input_html: {:value => current_organization_user.organizations.first.id}
+      end
+
       f.input :name
       f.input :end_date
       f.input :description, hint: "No more than 50 words. A shorter description performs better!", :input_html => {:rows => 2, :cols => 50}
@@ -20,6 +25,7 @@ ActiveAdmin.register FundingNeed, namespace: :myorg do
       f.input :primary_contact_name
       f.input :primary_contact_email
       f.input :primary_contact_phone
+      f.input :interests, hint: "Hold CTRL and click (CMD on the Mac) to select more than one item."
     end
     f.actions
   end
