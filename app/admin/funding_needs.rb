2346636f6902388adf05010733c616b692fb31af
Mairet, Tyler <tylermairet@gmail.com>
2018-03-24 14:51:57 -0500

    order orgs

--- app/admin/funding_needs.rb
+++ app/admin/funding_needs.rb
@@ -6,7 +6,7 @@ ActiveAdmin.register FundingNeed do
     f.inputs 'FundingNeed' do
       f.input :approved
       f.input :name
-      f.input :organization, include_blank: false
+      f.input :organization, :label => 'Organization', :as => :select, :collection => Organization.order(name: :asc).all.map{|o| [o.name, o.id]}, include_blank: false
       f.input :description
       f.input :website
       f.input :goal
