5463499f978a3935e561964bfd52577c7946c4b5
Luke Stadtler <luke@rocketreferrals.com>
2018-03-26 10:45:56 -0500

    search with middle search

--- app/controllers/funding_needs_controller.rb
+++ app/controllers/funding_needs_controller.rb
@@ -25,7 +25,7 @@ class FundingNeedsController < ApplicationController
     #TODO: do group by id and select count(*), weight by count(*) on matched_needs
     donor_interests = current_donor.field_of_interests.where(:liked => true).map{|interest| interest.interest_id}
     ids = if params[:search].present?
-            FundingNeed.joins('left join organizations on organizations.id = funding_needs.organization_id').where("organizations.name like ?", "#{params[:search]}%").where(:approved => true).where("end_date >= ?", Date.today).order(end_date: :asc).distinct.map{|need| need.id}
+            FundingNeed.joins('left join organizations on organizations.id = funding_needs.organization_id').where("organizations.name like ?", "%#{params[:search]}%").where(:approved => true).where("end_date >= ?", Date.today).order(end_date: :asc).distinct.map{|need| need.id}
           else
             FundingNeed.joins('join funding_needs_interests on funding_needs.id = funding_needs_interests.funding_need_id').where(:funding_needs_interests => {:interest_id => donor_interests}).where(:approved => true).where("end_date >= ?", Date.today).order(end_date: :asc).distinct.map{|need| need.id}
           end
